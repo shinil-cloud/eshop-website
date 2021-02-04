bd833f48cb5375ba22fa768430d5bab1180d7550
shinil <75559468+shinil-cloud@users.noreply.github.com>
2021-02-04 12:38:20 +0530

    Add files via upload

---
+++ store/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Product
+from .models import Category
+from .models import Customer
+from .models import Order
+
+
+
+# Register your models here.
+class AdminProduct(admin.ModelAdmin):
+    list_display=['name','price','category']
+
+class AdminCategory(admin.ModelAdmin):
+    list_display=['name']
+    
+
+
+
+
+admin.site.register(Product,AdminProduct)
+admin.site.register(Category,AdminCategory)
+admin.site.register(Customer)
+admin.site.register(Order)
