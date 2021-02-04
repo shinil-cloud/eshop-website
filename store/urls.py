7d4808540a561ab4f30546e3c9a30232bf2c26e5
shinil <75559468+shinil-cloud@users.noreply.github.com>
2021-02-04 12:40:56 +0530

    Add files via upload

---
+++ store/urls.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.urls import path
+from store import views
+from .views import Index,Signup,Login,logout,Cart,CheckOut,OrderView
+
+
+
+
+
+urlpatterns=[
+    path('',Index.as_view(),name="home"),
+    path('signup',Signup.as_view(),name='signup'),
+    path('login',Login.as_view(),name='login'),
+    path('logout',logout,name='logout'),
+    path('cart',Cart.as_view(),name='cart'),
+    path('check-out',CheckOut.as_view(),name='checkout'),
+    path('orders',OrderView.as_view(),name='orders'),
+
+   
+
+
+]
+    
